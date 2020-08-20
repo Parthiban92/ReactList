ce6e44afc851f939b0d1d2ee0c0f5bd69053d1d4
Parthiban <parthiban.n@navoinc.com>
2020-08-20 12:05:16 +0530

    Updates

--- precache-manifest.45cfdd0df35742261683893d953acca9.js
+++ precache-manifest.45cfdd0df35742261683893d953acca9.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "336273d25babf0f2ddb5f39a4bd2b000",
+    "revision": "0a37c735e84f5fc42086440728f2d093",
     "url": "/ReactList/index.html"
   },
   {
-    "revision": "558ae605d56e6a7d706e",
+    "revision": "bdedcfec7d6d95b3de51",
     "url": "/ReactList/static/css/main.07d5aa95.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/ReactList/static/js/2.5fdfa8c8.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "558ae605d56e6a7d706e",
-    "url": "/ReactList/static/js/main.428cf545.chunk.js"
+    "revision": "bdedcfec7d6d95b3de51",
+    "url": "/ReactList/static/js/main.02496511.chunk.js"
   },
   {
     "revision": "7a22fccbfdb440be8c4e",
